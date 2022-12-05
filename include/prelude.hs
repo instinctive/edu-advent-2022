2b64e1546afa267180e10f9f62cd49b4c1e46a4a
instinctive <177694+instinctive@users.noreply.github.com>
2022-12-05 15:50:07 -0800

    Add the State monad

--- include/prelude.hs
+++ include/prelude.hs
@@ -1,11 +1,16 @@
 module Main where
 
 import Control.Lens
+import Control.Monad.State
 import Linear.V2
 
 import Data.List.Split ( chunksOf, splitOn )
 
-import qualified Data.Set as S
+import Data.IntMap     ( IntMap )
+import Data.Set        ( Set    )
+
+import qualified Data.IntMap as IM
+import qualified Data.Set    as S
 
 paragraphs :: [String] -> [[String]]
 paragraphs = splitOn [""]
