b72613e070efdac0db9aa76cc49ee117ab2b4758
instinctive <177694+instinctive@users.noreply.github.com>
2022-12-06 12:16:22 -0800

    Add arrays

--- include/prelude.hs
+++ include/prelude.hs
@@ -2,13 +2,16 @@ module Main where
 
 import Control.Lens
 import Control.Monad.State
+import Data.Array.MArray
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.Array.Unsafe
+import Data.List.Split
 import Linear.V2
 
-import Data.List.Split ( chunksOf, splitOn )
-
-import Data.IntMap     ( IntMap )
-import Data.Map        ( Map    )
-import Data.Set        ( Set    )
+import Data.IntMap   ( IntMap   )
+import Data.Map      ( Map      )
+import Data.Set      ( Set      )
 
 import qualified Data.IntMap as IM
 import qualified Data.Map    as M
@@ -16,3 +19,6 @@ import qualified Data.Set    as S
 
 paragraphs :: [String] -> [[String]]
 paragraphs = splitOn [""]
+
+modifyArray :: (MArray a e m, Ix i) => a i e -> i -> (e -> e) -> m ()
+modifyArray a i f = readArray a i >>= writeArray a i . f
