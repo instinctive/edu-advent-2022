7c611e8f6fdc7c9e0d1d581e751bd4058e9a2e02
instinctive <177694+instinctive@users.noreply.github.com>
2022-12-06 10:55:14 -0800

    Add Map to prelude

--- include/prelude.hs
+++ include/prelude.hs
@@ -7,9 +7,11 @@ import Linear.V2
 import Data.List.Split ( chunksOf, splitOn )
 
 import Data.IntMap     ( IntMap )
+import Data.Map        ( Map    )
 import Data.Set        ( Set    )
 
 import qualified Data.IntMap as IM
+import qualified Data.Map    as M
 import qualified Data.Set    as S
 
 paragraphs :: [String] -> [[String]]
